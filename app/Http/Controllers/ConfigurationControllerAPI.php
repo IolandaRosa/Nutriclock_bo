0be719bf1dd2bf81d7a5282e10916c189b70b1eb
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2020-12-03 20:53:17 +0000

    Add configuration backend

--- app/Http/Controllers/ConfigurationControllerAPI.php
+++ app/Http/Controllers/ConfigurationControllerAPI.php
@@ -2,17 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Configuration;
+use App\Http\Resources\Configuration as ConfigurationResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 
 class ConfigurationControllerAPI extends Controller
 {
-    public function getTipEnabledValue()
+    public function index()
     {
-        //
+        return ConfigurationResource::collection(Configuration::all());
     }
 
-    public function setTipEnabledValue()
+    public function updateTipEnabled(Request $request)
     {
+        if (Auth::guard('api')->user()->role != 'ADMIN') {
+            return Response::json(['error' => 'Accesso proibido!'], 401);
+        }
 
+        $request->validate([
+            'value' => 'required',
+        ]);
+
+        $configuration = Configuration::where('key', 'SLEEP_TIP_ENABLED')->first();
+
+        if (!$configuration) {
+            return Response::json(['error' => 'A configuração não existe!'], 404);
+        }
+
+        $configuration->value = $request->input('value');
+        $configuration->save();
+        return new ConfigurationResource($configuration);
     }
 }
