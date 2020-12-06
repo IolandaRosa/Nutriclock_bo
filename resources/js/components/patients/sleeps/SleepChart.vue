0b21e4f6ec2d0127d02e7bd48299c291a4feb8bc
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2020-12-06 13:04:37 +0000

    force yy axis to begin at min

--- resources/js/components/patients/sleeps/SleepChart.vue
+++ resources/js/components/patients/sleeps/SleepChart.vue
@@ -77,6 +77,7 @@
                            ,
                            ticks: {
                                 beginAtZero: true,
+                                min: 0,
                            }
                         }]
                     }
