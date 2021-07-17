39d764261830b5673004b92f1315baebd5099ca9
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2021-07-17 23:01:17 +0100

    Change some bugs reported

--- app/Jobs/Notifications.php
+++ app/Jobs/Notifications.php
@@ -35,33 +35,7 @@ class Notifications implements ShouldQueue
                 if ($u->fcmToken) {
                     $notifications = Notification::where('userId', $u->id)->first();
                     if ($notifications) {
-                        if ($notifications->notificationsBiometric) {
-                            $collectionsDates = BiometricCollections::where('biometric_group_id', $u->biometric_group_id)->get();
-                            $today = date("d-m-Y");
-
-                            foreach ($collectionsDates as $collection) {
-
-                                if ($collection->date == $today) {
-                                    $intervals = BiometricCollectionIntervals::where('collectionId', $collection->id)->get();
-
-                                    foreach ($intervals as $interval) {
-                                        $intervalHour = $interval->hour;
-
-                                        for ($i = 0; $i < 7 ; $i++) {
-                                            $minAdd = 10 + $i;
-                                            $minMinus = 10 - $i;
-                                            $intervalTimeMinus = date("H:i", strtotime($intervalHour . ' -1 hour' . ' -'.$minMinus.' minutes'));
-                                            $intervalTimeAdd = date("H:i", strtotime($intervalHour . ' -1 hour' . ' -'.$minAdd.' minutes'));
-                                            if ($intervalTimeMinus == $hour || $intervalTimeAdd == $hour) {
-                                                $u->notify(new FCMNotification($u->fcmToken, 'Recolha de Saliva', 'Prepare-se para realizar a próxima recolha de saliva às ' . $intervalHour . ' horas.'));
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        if ($hour > date('H:i', strtotime('12:00')) && $hour < date('H:i', strtotime('12:20'))) {
+                        if ($hour > date('H:i', strtotime('22:27')) && $hour < date('H:i', strtotime('22:37'))) {
                             if ($notifications->notificationsSleep) {
                                 $sleep = Sleep::where('userId', $u->id)->orderBy('date', 'desc')->first('date');
 
@@ -101,6 +75,33 @@ class Notifications implements ShouldQueue
                                     $mealDiaryDays = round(($now - $mealDiaryDate) / (60 * 60 * 24));
                                     if ($mealDiaryDays > 0 && $mealDiaryDays < 2) {
                                         $u->notify(new FCMNotification($u->fcmToken, 'Diário Alimentar', 'Não se esqueça de registar todas assuas refeições ao longo do dia'));
+                                    }
+                                }
+                            }
+                        }
+
+
+                        if ($notifications->notificationsBiometric) {
+                            $collectionsDates = BiometricCollections::where('biometric_group_id', $u->biometric_group_id)->get();
+                            $today = date("d-m-Y");
+
+                            foreach ($collectionsDates as $collection) {
+
+                                if ($collection->date == $today) {
+                                    $intervals = BiometricCollectionIntervals::where('collectionId', $collection->id)->get();
+
+                                    foreach ($intervals as $interval) {
+                                        $intervalHour = $interval->hour;
+
+                                        for ($i = 0; $i < 7 ; $i++) {
+                                            $minAdd = 10 + $i;
+                                            $minMinus = 10 - $i;
+                                            $intervalTimeMinus = date("H:i", strtotime($intervalHour . ' -1 hour' . ' -'.$minMinus.' minutes'));
+                                            $intervalTimeAdd = date("H:i", strtotime($intervalHour . ' -1 hour' . ' -'.$minAdd.' minutes'));
+                                            if ($intervalTimeMinus == $hour || $intervalTimeAdd == $hour) {
+                                                $u->notify(new FCMNotification($u->fcmToken, 'Recolha de Saliva', 'Prepare-se para realizar a próxima recolha de saliva às ' . $intervalHour . ' horas.'));
+                                            }
+                                        }
                                     }
                                 }
                             }
