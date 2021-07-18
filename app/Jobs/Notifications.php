6242a5f9a88a74e2722f4f64cef1ac0e4743dfc8
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2021-07-18 14:21:30 +0100

    Add test method for notifications

--- app/Jobs/Notifications.php
+++ app/Jobs/Notifications.php
@@ -35,7 +35,7 @@ class Notifications implements ShouldQueue
                 if ($u->fcmToken) {
                     $notifications = Notification::where('userId', $u->id)->first();
                     if ($notifications) {
-                        if ($hour > date('H:i', strtotime('12:00')) && $hour < date('H:i', strtotime('12:10'))) {
+                        if ($hour > date('H:i', strtotime('14:25')) && $hour < date('H:i', strtotime('14:40'))) {
                             if ($notifications->notificationsSleep) {
                                 $sleep = Sleep::where('userId', $u->id)->orderBy('date', 'desc')->first('date');
 
