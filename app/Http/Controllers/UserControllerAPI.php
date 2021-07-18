0a5ac2f2ecc663f76e22b4e0f3868d888f56b9e6
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2021-07-18 22:12:26 +0100

    Changed notifications

--- app/Http/Controllers/UserControllerAPI.php
+++ app/Http/Controllers/UserControllerAPI.php
@@ -1105,8 +1105,6 @@ class UserControllerAPI extends Controller
         $hour = date("H:i");
         $users = User::where('id', 49)->get();
 
-        return Response::json(['data' => $users]);
-
         if ($users) {
             foreach ($users as $u) {
                 if ($u->fcmToken) {
