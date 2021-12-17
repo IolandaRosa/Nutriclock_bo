c984c6775cc035ad2a5122243b3e043ffbd3ec26
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2021-12-17 23:21:42 +0000

    improve renderization of terms and conditions

--- resources/js/components/utils/TermsAndConditionsPublic.vue
+++ resources/js/components/utils/TermsAndConditionsPublic.vue
@@ -4,54 +4,90 @@
             <div class="d-flex mb-3 align-items-center">
                 <img :src="'images/logo_text_horizontal.png'" alt="" height="30"/>
                 <h1 class="text-dark font-weight-bold flex-grow-1 text-right mb-0">
-                    {{title}}
+                    Termos e Condições
                 </h1>
             </div>
+            <h4 class="text-dark text-justify">
+                Bem-vindo ao NutriClock!
+            </h4>
             <h6 class="text-dark text-justify">
-                {{description}}
+                Estes termos e condições descrevem as regras e regulamento para o uso da aplicação móvel NutriClock. Ao utilizar esta aplicação, assume-se que aceita estes termos e condições. Não continue a usar a aplicação NutriClock se não concordar com todos os termos e condições indicados nesta página.
+            </h6>
+            <h5 class="text-dark text-justify">
+                Recolha de dados
+            </h5>
+            <h6 class="text-dark text-justify">
+                A aplicação NutriClock foi desenvolvida no âmbito de um projeto de investigação.
+                Todos os dados recolhidos pela aplicação respeitam a nossa política de privacidade.
+                A aplicação NutriClock irá recolher as seguintes informações sobre si:
+            </h6>
+            <h6 class="text-dark text-justify">
+                • Detalhes do login (endereço de e-mail e nome de utilizador). <br/>
+                • Dados pessoais, como o seu nome, sexo, peso, altura, histórico de doenças e alergias/intolerâncias alimentares, terapêutica farmacológica e uso de suplementos alimentares. <br/>
+                • Hábitos alimentares, hábitos de sono e exercício físico.<br/>
+                • Informações sobre a utilização da aplicação móvel.
+            </h6>
+
+
+            <h5 class="text-dark text-justify">
+                Processamento dos dados
+            </h5>
+            <h6 class="text-dark text-justify">
+                Estas informações serão utilizadas para os seguintes fins:
+            </h6>
+            <h6 class="text-dark text-justify">
+                • Armazenar os seus dados com segurança, permitindo que aceda a qualquer momento no futuro, se desejar ter acesso aos mesmos. <br/>
+                • Permitir que utilize os recursos e serviços disponíveis na aplicação NutriClock. <br />
+                • Utilizar os dados em investigação, salvaguardando os princípios de anonimização e confidencialidade.
+            </h6>
+
+            <h5 class="text-dark text-justify">
+                Acesso aos seus dados
+            </h5>
+            <h6 class="text-dark text-justify">
+                Pode aceder a todos os dados que lhe pertencem e que são armazenados na aplicação NutriClock.
+                Para isso, envie um e-mail para <a  class="d-inline text-info" href="mailto:marlene.c.lages@ipleiria.pt">marlene.c.lages@ipleiria.pt</a> com a solicitação.
+                Neste e-mail, indique que gostaria de receber uma visão geral dos seus dados e deve ainda indicar o endereço de e-mail da sua conta NutriClock.
+                As informações ser-lhe-ão enviadas o mais brevemente possível.
+                Na aplicação tem a possibilidade de rever alguns dos dados introduzidos na forma de relatório.
+                Para isso, basta aceder ao menu lateral e clicar em “Relatórios”.
+                Os dados pessoais, como o seu nome e endereço de e-mail apenas podem ser acedidos pela equipa NutriClock que garantem a anonimidade e confidencialidade dos mesmos.
+            </h6>
+
+            <h5 class="text-dark text-justify">
+                Eliminar os seus dados
+            </h5>
+            <h6 class="text-dark text-justify">
+                Pode também indicar que não gostaria de usar mais o NutriClock e que deseja que os seus dados sejam removidos. Para isso, envie um e-mail para <a class="d-inline text-info" href="mailto:marlene.c.lages@ipleiria.pt">marlene.c.lages@ipleiria</a> com a solicitação. Neste e-mail, indique que deseja que a sua conta e os seus dados sejam excluídos. Mencione qual o endereço de e-mail da sua conta NutriClock. Após a receção do seu e-mail, os seus dados serão eliminados e receberá uma confirmação sobre a remoção dos seus dados o mais rápido possível. Pode igualmente eliminar os seus dados através das definições da aplicação NutriClock. Para isso, basta clicar em “Esquecer os meus dados” e confirmar a ação.
+            </h6>
+
+            <h5 class="text-dark text-justify">
+                Licença
+            </h5>
+            <h6 class="text-dark text-justify">
+                Salvo indicação em contrário, os licenciadores do NutriClock detêm os direitos de propriedade intelectual de todo o material do NutriClock. Todos os direitos de propriedade intelectual são reservados. Pode aceder à aplicação NutriClock para o seu uso pessoal sujeito às restrições estabelecidas nestes termos e condições. Não deve:
+            </h6>
+            <h6 class="text-dark text-justify">
+                • Reeditar o material da aplicação NutriClock<br/>
+                • Vender, alugar ou sublicenciar o material da aplicação NutriClock<br />
+                • Reproduzir, duplicar ou copiar o material da aplicação NutriClock<br />
+                • Redistribuir o conteúdo da aplicação NutriClock
+            </h6>
+
+            <h6 class="text-dark text-justify">
+                A sua <b>Privacidade</b> É garantida a confidencialidade de todos os dados introduzidos na aplicação NutriClock e o acesso aos mesmo será restrito aos investigadores, que estão obrigados a manter a confidencialidade e anonimato dos dados. Todos os ficheiros eletrónicos gerados terão restrição de acesso aos conteúdos, impedindo que as informações confidenciais sejam acedidas, impressas, reencaminhadas ou copiadas por pessoas não autorizadas.
+                 A sua identidade nunca será revelada em quaisquer relatórios ou publicações científicas.
             </h6>
         </div>
     </div>
 </template>
 
 <script type="text/javascript">
-import {ERROR_MESSAGES, isEmptyField} from '../../utils/validations';
-import {ROUTE} from '../../utils/routes';
-
 export default {
     data: function () {
         return {
-            title: '',
-            version: '',
-            description: '',
-            errors: {
-                title: null,
-                description: null,
-            },
-            isFetching: false,
-        };
-    },
-    methods: {
-        getTermsAndConditions() {
-            if (this.isFetching) return;
-            this.isFetching = true;
 
-            axios.get('api/terms').then(response => {
-                this.isFetching = false;
-                this.title = response.data.data.title;
-                this.description = response.data.data.description;
-                this.version = response.data.data.version;
-            }).catch(error => {
-                this.isFetching = false;
-                if (error.response && error.response.status === 401) {
-                    this.$store.commit('clearUserAndToken');
-                    this.$router.push({path: ROUTE.Login });
-                }
-            });
-        }
-    },
-    mounted() {
-        this.getTermsAndConditions();
-    },
+        };
+    }
 };
 </script>
